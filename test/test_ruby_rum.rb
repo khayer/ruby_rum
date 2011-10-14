55db2c868cf1c298890f94b6694c9a3bbf355e7b
Kaharina Hayer <katharinaehayer@gmail.com>
2011-10-14 13:39:59 -0400

    The executables are more ruby like now…

--- test/test_ruby_rum.rb
+++ test/test_ruby_rum.rb
@@ -10,11 +10,11 @@ class MyUnitTests < Test::Unit::TestCase
 		@content = RubyRum::BowtieContent.new("r14/1	0	gi|110640213|ref|NC_008253.1|	39899	255	35M	*	0	0	GTAATTTGAGTAATGCCCACCAGTTCCATCACGAT	EDCCCBAAAA@@@@?>===<;;9:99987776554	XA:i:0	MD:Z:35	NM:i:0")
 	end
 
-	def test_initialize_bowtie_content()
+	def test_bowtie_content_initialize()
 		assert_equal(@content.seq,"GTAATTTGAGTAATGCCCACCAGTTCCATCACGAT" )
 	end
 
-	def test_to_s_bowtie_content()
+	def test_bowtie_content_to_s()
 		str = @content.to_s()
 		assert_equal(str, "r14/1	0	gi|110640213|ref|NC_008253.1|	39899	255	35M	*	0	0	GTAATTTGAGTAATGCCCACCAGTTCCATCACGAT	EDCCCBAAAA@@@@?>===<;;9:99987776554\n" )
 	end
@@ -28,10 +28,15 @@ class MyUnitTests < Test::Unit::TestCase
 	def test_call_bowtie()
 		out = "e_coli_result"
 		para = RubyRum::PreBowtie.new("/bowtie/bowtie", "/bowtie/indexes/e_coli", "/bowtie/reads/e_coli_1000_1.fq", "/bowtie/reads/e_coli_1000_2.fq", out )
-	   	assert_equal(File.exist?(out), true)
-	   	para2 = RubyRum::PreBowtie.new("/bowtie/reads/e_coli_100_1.fq", "/bowtie/reads/e_coli_1000_2.fq", "/bowtie/indexes/e_coli", "/bowtie/bowtie", out )
-	   	out2 = "e_coli_result2"
-	   	assert_equal(File.exist?(out2), false)
+   	assert_equal(File.exist?(out), true)
+   	out2 = "e_coli_result2"
+   	para2 = RubyRum::PreBowtie.new("/bowtie/reads/e_coli_100_1.fq", "/bowtie/reads/e_coli_1000_2.fq", "/bowtie/indexes/e_coli", "/bowtie/bowtie", out2 )
+   	assert_equal(File.exist?(out2), false)
+
+   	# This is working, but taking to much time...
+   	#out3 = "hg_19_result"
+   	#para2 = RubyRum::PreBowtie.new("/bowtie/reads/example2_R1_001.fastq", "/bowtie/reads/example2_R2_001.fastq", "/bowtie/indexes/hg19_genome", "/bowtie/bowtie", out3 )
+   	#assert_equal(File.exist?(out3), true)
 	end
 
 	def test_bowtie_parser_open()
