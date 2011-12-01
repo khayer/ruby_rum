f15a455bdc59f6c2fa8c1df5604841f66914ead4
Katharina Hayer <katharinaehayer@gmail.com>
2011-12-01 13:10:49 -0500

    Updated bin methods

--- lib/ruby_rum/fastq.rb
+++ lib/ruby_rum/fastq.rb
@@ -2,6 +2,30 @@ require "ruby_rum/fastq/fastq_parser"
 require "ruby_rum/fastq/fastq_entry"
 
 module Fastq
+  def combine()
+    STDERR.puts "Combining of Fastq is not supported!"
+  end
+
+  def combine_seriell(filename1, filename2, outdir)
+    f1 = ::File.new(filename1, 'r')
+    f2 = ::File.new(filename2, 'r')
+    out = ::File.new(outdir,'w')
+    i = 0
+    r1 = ""
+    r2 = ""
+    while !f2.eof?()
+      while i < 4
+        r1 += f1.readline()
+        r2 += f2.readline()
+        i += 1
+      end
+      i = 0
+      str = r1+r2
+      out.write(str)
+      r1 = ""
+      r2 = ""
+    end
+  end
 
 end
 
